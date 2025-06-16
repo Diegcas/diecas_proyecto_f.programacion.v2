6f5cd227f3213ba04987cc19238589fe528f02e0
Diegcas <die.castillov@duocuc.cl>
2025-06-16 14:19:22 -0400

    Add files via upload

---
+++ #Convertir un lista a tupla.py
@@ -0,0 +1,5 @@
+#Convertir un lista a tupla
+mi_lista = [1, 2, 3]
+mi_tupla = tuple(mi_lista)
+
+print(mi_tupla)
---
+++ #diccionario.py
@@ -0,0 +1,7 @@
+#diccionario
+diccionario = {"rut: 213250264"
+                "nombre":"cesar huispe",
+               "ubicacion: plaza oeste"
+               "fonos":[988778882,98887778882,8777666333],
+               "activo": True}
+print(diccionario)
